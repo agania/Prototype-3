41a97180b6646361a024860cd0c7aa0c175c2cd9
Piotr <aldegard@yahoo.com>
2020-06-17 23:41:03 +0200

    2222

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -7,18 +7,19 @@ public class SpawnManager : MonoBehaviour
     public GameObject obstaclePrefab;
     public GameObject backgroundPrefab;
 
-    private Vector3 spawnPos = new Vector3(25, 0, 0);
+    private Vector3 spawnPos = new Vector3(225, 0, 0);
     private float startDelay = 2.0f;
     private float repeatRate = 2.0f;
-    private Vector3 spawnPos2 = new Vector3(157, 9, 4);
-//    private float startDelay2 = 2.0f;
+    private Vector3 buildingsSpawnPos = new Vector3(157, 9, 10);
+    private Vector3 buildings2SpawnPos = new Vector3(157, 9, -12);
+    //    private float startDelay2 = 2.0f;
     public float repeatRate2 = 12.0f;
 
     // Start is called before the first frame update
     void Start()
     {
         InvokeRepeating("SpawnObstacle", startDelay, repeatRate);
-        InvokeRepeating("SpawnBackground", 0, repeatRate2);
+        InvokeRepeating("SpawnBackground", 0, repeatRate2);//startDelay2 -> 0
     }
 
     // Update is called once per frame
@@ -33,6 +34,7 @@ public class SpawnManager : MonoBehaviour
     }
     void SpawnBackground()
     {
-        Instantiate(backgroundPrefab, spawnPos2, backgroundPrefab.transform.rotation);
+        Instantiate(backgroundPrefab, buildingsSpawnPos, backgroundPrefab.transform.rotation);
+        Instantiate(backgroundPrefab, buildings2SpawnPos, backgroundPrefab.transform.rotation);
     }
 }
